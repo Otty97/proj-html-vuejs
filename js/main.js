56fff400fe2624e5989654fe80b5808b45f08556
Othman Taik <taikothman1997@gmail.com>
2020-12-11 16:54:42 +0100

    Vue.js added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 const remoteTrainig = new Vue({
   el:'#root',
-  data:{},
-  methods:{},
+  data:{
+    contact:{
+     adress: '382 NE 191st St # 87394 Miami, FL 33179',
+     number: '+1(305) 547-9909',
+     aviability: '9am -5pm EST, Monday - Friday',
+     email:'support@maxcoach.com',
+    },
+  },
+  methods:{
+    anchorPage: function(){
+      //router-link
+    }
+  },
 })
