2cca9bfecee301842e1d2134f539b3fc707adcf5
Unknown <taikothman1997@gmail.com>
2020-12-14 11:56:28 +0100

    Project Completed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 const remoteTrainig = new Vue({
   el:'#root',
   data:{
+    navBar:["Home", "Pages", "Courses", "Feautures", "Blog", "Shop"],
     contact:{
      adress: '382 NE 191st St # 87394 Miami, FL 33179',
      number: '+1(305) 547-9909',
      aviability: '9am -5pm EST, Monday - Friday',
      email:'support@maxcoach.com',
     },
-  },
-  methods:{
-    anchorPage: function(){
-      //router-link
-    }
   },
 })
